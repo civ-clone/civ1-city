e627901fd7347fe01f3b51472f7a926a14bbccd9
dom111 <dom111@users.noreply.github.com>
2021-02-18 14:09:31 +0000

    Add missing `Rule`s from `registerRules`.

--- registerRules.js
+++ registerRules.js
@@ -6,9 +6,11 @@ const captured_1 = require("./Rules/City/captured");
 const cost_1 = require("./Rules/City/cost");
 const created_1 = require("./Rules/City/created");
 const destroyed_1 = require("./Rules/City/destroyed");
+const food_storage_1 = require("./Rules/City/food-storage");
 const grow_1 = require("./Rules/City/grow");
+const growth_cost_1 = require("./Rules/City/growth-cost");
 const action_1 = require("./Rules/Player/action");
 const process_yield_1 = require("./Rules/City/process-yield");
 const shrink_1 = require("./Rules/City/shrink");
-RuleRegistry_1.instance.register(...building_complete_1.default(), ...captured_1.default(), ...cost_1.default(), ...created_1.default(), ...destroyed_1.default(), ...grow_1.default(), ...action_1.default(), ...process_yield_1.default(), ...shrink_1.default());
+RuleRegistry_1.instance.register(...building_complete_1.default(), ...captured_1.default(), ...cost_1.default(), ...created_1.default(), ...destroyed_1.default(), ...food_storage_1.default(), ...grow_1.default(), ...growth_cost_1.default(), ...action_1.default(), ...process_yield_1.default(), ...shrink_1.default());
 //# sourceMappingURL=registerRules.js.map
--- registerRules.ts
+++ registerRules.ts
@@ -4,7 +4,9 @@ import captured from './Rules/City/captured';
 import cost from './Rules/City/cost';
 import created from './Rules/City/created';
 import destroyed from './Rules/City/destroyed';
+import foodStorage from './Rules/City/food-storage';
 import grow from './Rules/City/grow';
+import growthCost from './Rules/City/growth-cost';
 import playerAction from './Rules/Player/action';
 import processYield from './Rules/City/process-yield';
 import shrink from './Rules/City/shrink';
@@ -15,7 +17,9 @@ ruleRegistryInstance.register(
   ...cost(),
   ...created(),
   ...destroyed(),
+  ...foodStorage(),
   ...grow(),
+  ...growthCost(),
   ...playerAction(),
   ...processYield(),
   ...shrink()
